b8c6ba9543d97b1a5727c5b8c2f53510244c916b
Jonathan.Fletcher <jonathan.fletcher@garmin.com>
2024-11-22 18:16:40 +0000

    Upload Jar to Veracode

--- src/main/java/com/garminpay/model/SDKVersion.java
+++ src/main/java/com/garminpay/model/SDKVersion.java
@@ -10,5 +10,5 @@ public final class SDKVersion {
     private SDKVersion() {
     }
 
-    public static final String VERSION = "0.8.1-SNAPSHOT";
+    public static final String VERSION = "0.9.0-SNAPSHOT";
 }
